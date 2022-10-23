2b29884e5273f44392714a654c1264169dc4f0e8
cintyy <cintiapaezdm@gmail.com>
2022-10-23 03:19:29 -0300

    modal img ok! agrego paleta de colores falta revisar add remove en js

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const $ = (selector) => document.querySelector(selector);
 // *****Elementos
 const btnClaro = $(".modo-claro");
 const main = $("main");
-const header =$("header");
-const modalImg =$(".form-img"); 
+const header = $("header");
+const modalImg = $(".form-img");
 const btnImg = $(".btn-img");
-const btnX  =$("btn-x")
-console.log(modalImg)
-console.log(btnImg)
+const btnX  =$(".btn-x");
+
+const contPaleta = $("#cont-paleta-color");
+console.log(contPaleta)
+const color = $(".color");
+
+const cancelar = $(".cancelar");
+console.log(cancelar)
+
+
+// console.log(modalImg)
+// console.log(btnImg)
 /******Eventos*****/
 
 /*---- --------Modo claro/oscuro-------------*/
@@ -20,7 +29,6 @@ btnClaro.addEventListener("click", ()=>{
      
     if(btnClaro.innerText==="😎 Modo claro"){
         btnClaro.innerText="🌚 Modo oscuro";
-
     }
     else{
         btnClaro.innerText="😎 Modo claro"
@@ -29,15 +37,27 @@ btnClaro.addEventListener("click", ()=>{
 
 /*-----modal configuracion img---------*/
 
+
+
 btnImg.addEventListener("click", ()=>{
     modalImg.classList.remove("close");
     modalImg.classList.add("open");
-    console.log(modalImg)
-    console.log(btnImg)
+
 })
 
 btnX.addEventListener("click", ()=>{
     modalImg.classList.add("close");
     modalImg.classList.remove("open");
+
+})
+
+color.addEventListener("click", ()=>{
+    contPaleta.classList.remove("close");
+    contPaleta.classList.add("open");
+})
+
+cancelar.addEventListener("click", ()=>{
+    contPaleta.classList.add("close");
+    contPaleta.classList.remove("open");
 
 })
