9102533ad60449d96375d9176dbada638b78398a
cintyy <cintiapaezdm@gmail.com>
2022-11-02 17:57:30 -0300

    aplico eventos a los botones del menu texto

--- app.js
+++ app.js
@@ -11,9 +11,31 @@ const btnLi = $(".hover");
 const contImg = $(".container-img");
 const urlImg = $("#url-img");
 const imgFiltros = $("#imgFiltros");
+const colorP = $(".sexag"); 
+const colorPtxt = $(".sexag-txt"); 
+const colorPbg = $(".sexag-txt-bg"); 
+const topT = $(".txt-top");
+const botT =$(".txt-bottom");
+const txtTopI = $("#top-text");
+const txtBottomI = $("#bottom-text");
+const fuente = $("#fuente");
+const size = $("#font-size");
+const btnNinguno = $(".btn-contorno-ninguno");
+const btnContClaro = $(".btn-contorno-claro");
+const btnOscuro = $(".btn-contorno-oscuro");
+const btnCentrado = $("#centrado");
+const btnIzquierda = $("#izquierda");
+const btnDerecha = $("#derecha");
+const espaciadoI = $("#espaciado");
+const interlineadoA = $("#interlineadoA");
+const interlineadoB = $("#interlineadoB");
+const interlineadoC = $("#interlineadoC");
+const interlineadoD = $("#interlineadoD");
+const interlineadoE = $("#interlineadoE");
+const interlineadoF = $("#interlineadoF");
 // filtros
+const opcionesImg = $("#option-color");
 const brillo = $("#brillo");
-console.log(brillo)
 const opacidad = $("#opacidad");
 const contraste = $("#contraste");
 const desenfoque = $("#desenfoque");
@@ -22,8 +44,8 @@ const sepia = $("#sepia");
 const hue = $("#hue");
 const saturado = $("#saturado");
 const negativo = $("#negativo");
-const contTxtBottom = $(".bottom-text");
 const contTxtTop = $(".top-text");
+const contTxtBottom = $(".bottom-text");
 const circulo = $(".circulo");
 const btnAbrir = $("#abrir-paleta");
 const contPaleta = $("#cont-paleta-color");
@@ -34,6 +56,7 @@ const contInputColor = $(".cont-est-canc");
 const divInputColor = $(".cont-input-color");
 const btnEstablecer = $(".establecer");
 const reestablecerF = $(".reestablecer-f");
+const sinBg = $("#bg-transp");
 const paleta = $(".paleta");
 const red = $(".red");
 const lBlue = $(".l-blue");
@@ -52,7 +75,8 @@ const contPaletaTxt = $("#cont-paleta-color-txt");
 const cancelarTxt = $(".cancelar-txt");
 const btnEstablecerTxt = $(".establecer-txt");
 const divInputColorTxt = $(".cont-input-color-txt");
-
+const txtTopNone = $("#txt-none");
+const txtBottomNone = $("#txt-bottom-none");
 const redTxt = $(".red-txt");
 const lBlueTxt = $(".l-blue-txt");
 const blueTxt = $(".blue-txt");
@@ -67,12 +91,9 @@ const circuloTxtBg = $(".circulo-txt-bg");
 const elegidoInputColorTxtBg = $("#input-color-txt-bg");
 const contPaletaBg = $("#cont-paleta-color-txt-bg");
 const btnAbrirBg = $("#abrir-paleta-txt-bg");
-
 const cancelarBg = $(".cancelar-txt-bg");
 const btnEstablecerBg = $(".establecer-txt-bg");
 const divInputColorBg = $(".cont-input-color-txt-Bg");
-// console.log(divInputColorBg)
-
 const redBg = $(".red-bg");
 const lBlueBg = $(".l-blue-bg");
 const blueBg = $(".blue-bg");
@@ -81,7 +102,6 @@ const pinkBg = $(".pink-bg");
 const yellowBg = $(".yellow-bg");
 const blackBg = $(".black-bg");
 const whiteBg = $(".white-bg");
-// const btnEstablecerBg = $(".establecer");
 const dowloand = $(".dowloand");
 const containerImg = $(".container-img")
 const btnTxt= $(".btn-txt")
@@ -89,28 +109,69 @@ const modalTxt = $("#form-txt")
 const btnXtxt = $(".btn-x-txt")
 
 /*---- --------FUNCIONES-------------*/
-// color
+
+//modo claro/oscuro
+const modo = () => {
+    main.classList.toggle("modoClaro");
+    header.classList.toggle("modoClaroHeader");
+    
+    if (btnClaro.innerText === "😎 Modo claro") {
+        btnClaro.innerText = "🌚 Modo oscuro";
+    }
+    else {
+        btnClaro.innerText = "😎 Modo claro"
+    }
+}
+//opciones img
+const aplicarModosImg = (e) =>{
+    let opcionModo = e.target.value;
+    console.log(opcionModo);
+    imgFiltros.style.backgroundBlendMode= opcionModo
+}
+opcionesImg.addEventListener("click",aplicarModosImg);
+//fuentes 
+const aplicarFuente = (e) =>{
+    fuenteAplicada = e.target.value;
+    topT.style.fontFamily = fuenteAplicada;
+    botT.style.fontFamily = fuenteAplicada;
+    
+}
+//tamaño fuente
+const aplicarTamaño = (e)=>{
+    valorTamaño = size.value;
+    topT.style.fontSize = `${valorTamaño}px`
+    botT.style.fontSize = `${valorTamaño}px`
+}
+
+// color fondo img
 const cambiarFondo = () => {
     let valorColorInput = elegidoInputColor.value;
+    colorP.innerText = valorColorInput;
     colorElegido.style.backgroundColor = valorColorInput;
     circulo.style.backgroundColor = valorColorInput;
     contImg.style.backgroundColor = valorColorInput;
 }
 const cambiarFondoDiv = (color) => {
+    colorP.innerText = color;
     colorElegido.style.backgroundColor = color
     circulo.style.backgroundColor = color
     contImg.style.backgroundColor = color
 }
 
+// color texto div
 const cambiarColorTxt = (color) => {
+    colorPtxt.innerText = color;
     colorElegidoTxt.style.backgroundColor = color
     circuloTxt.style.backgroundColor = color
     contTxtTop.style.color = color
     contTxtBottom.style.color = color
 }
+
+// color texto input
 const cambiarColorTxtI = () => {
     console.log("funciona")
     let valorColorInputTxt = elegidoInputColorTxt.value;
+    colorPtxt.innerText = valorColorInputTxt;
     console.log(valorColorInputTxt)
     colorElegidoTxt.backgroundcolor = valorColorInputTxt;
     circuloTxt.style.backgroundColor = valorColorInputTxt;
@@ -118,21 +179,26 @@ const cambiarColorTxtI = () => {
     contTxtTop.style.color = valorColorInputTxt;
 }
 
+//color fondo div
 const cambiarColorBgTxt = (color) => {
+    colorPbg.innerText = color;
     colorElegidoTxtBg.style.backgroundColor = color;
     circuloTxtBg.style.backgroundColor = color;
     contTxtTop.style.backgroundColor = color;
     contTxtBottom.style.backgroundColor = color;
 }
+
+//color fondo div input
 const cambiarBgTxtI = () => {
     let valorColorInputTxtBg = elegidoInputColorTxtBg.value;
+    colorPbg.innerText = valorColorInputTxtBg;
     colorElegidoTxtBg.style.backgroundColor = valorColorInputTxtBg;
     circuloTxtBg.style.backgroundColor = valorColorInputTxtBg;
     contTxtBottom.style.backgroundColor = valorColorInputTxtBg;
     contTxtTop.style.backgroundColor = valorColorInputTxtBg;
 }
 
-// img
+// insertar img
 const actualizarImg = (e)=>{
     console.log(e.target.value)
     console.log(contImg)
@@ -140,6 +206,41 @@ const actualizarImg = (e)=>{
     <div class="style-img">
     <img class="style-img" id="imgFiltros" src="${e.target.value}" alt="">
     </div>`
+}
+
+//contorno texto
+const sinContorno = ()=>{
+    topT.classList.remove("contClaro");
+    topT.classList.remove("contOscuro");
+    botT.classList.remove("contClaro");
+    botT.classList.remove("contOscuro")
+}
+
+const contornoClaro = () =>{
+    topT.classList.add("contClaro");
+    botT.classList.add("contClaro");
+    topT.classList.remove("contOscuro");
+    botT.classList.remove("contOscuro")
+}
+
+const contornoOscuro =  ()=>{
+    botT.classList.add("contOscuro")
+    topT.classList.add("contOscuro");
+    topT.classList.remove("contClaro");
+    botT.classList.remove("contClaro");
+}
+
+// fondo transparente
+
+const transpFondo = () =>{
+    if(sinBg.checked == true){
+        contTxtTop.style.background = "transparent";
+        contTxtBottom.style.background = "transparent";
+    }
+    else{
+        contTxtTop.style.backgroundColor = colorPbg.innerText;
+        contTxtBottom.style.backgroundColor=colorPbg.innerText;
+    }
     
 }
 // filtros
@@ -153,8 +254,8 @@ const aplicarF = ( ) =>{
     let hueV = hue.value;
     let saturadoV = saturado.value;
     let negativoV = negativo.value;
-    imgFiltros.style.filter = `brigthness(${brilloV}) opacity(${opacidadV}) contrast(${contrasteV}%) blur(${desenfoqueV}px) grayscale(${grisesV}%) sepia(${sepiaV}%) hue-rotate(${hueV}deg) saturate(${saturadoV}%) invert(${negativoV});`
-} //me tira error rn style                                                                                                                                                                                                                                                            
+    // imgFiltros.style.filter = `brigthness(${brilloV}) opacity(${opacidadV}) contrast(${contrasteV}%) blur(${desenfoqueV}px) grayscale(${grisesV}%) sepia(${sepiaV}%) hue-rotate(${hueV}deg) saturate(${saturadoV}%) invert(${negativoV});`
+} //me tira error rn style                     
 
 // btn descarga
 const dowloandMeme =  () =>{
@@ -166,18 +267,8 @@ const dowloandMeme =  () =>{
 /******Eventos*****/
 
 /*---- --------Modo claro/oscuro-------------*/
+btnClaro.addEventListener("click", modo)
 
-btnClaro.addEventListener("click", () => {
-    main.classList.toggle("modoClaro");
-    header.classList.toggle("modoClaroHeader");
-    
-    if (btnClaro.innerText === "😎 Modo claro") {
-        btnClaro.innerText = "🌚 Modo oscuro";
-    }
-    else {
-        btnClaro.innerText = "😎 Modo claro"
-    }
-})
 
 /*-----modal configuracion img---------*/
 
@@ -191,13 +282,10 @@ btnX.addEventListener("click", () => {
     modalImg.classList.remove("open");
 })
 
-
-
-/*****url image****** */
+/*****url imagen****** */
 urlImg.addEventListener("input",actualizarImg)
 
 //CONTAINER COLOR
-
 btnAbrir.addEventListener("click", () => {
     contPaleta.classList.toggle("close");
     contPaleta.classList.toggle("open");
@@ -219,9 +307,7 @@ reestablecerF.addEventListener("click",()=>{
     contImg.classList.add("reestablecerF");
 })
 
-// })
-
-
+//color texto
 divInputColor.addEventListener("change", (e) => {
     cambiarFondo();
 });
@@ -257,9 +343,6 @@ black.addEventListener("click", () => {
 white.addEventListener("click", () => {
     cambiarFondoDiv("white");
 })
-
-
-
 
 /***********Dowloand********/
 dowloand.addEventListener("click",dowloandMeme)
@@ -314,9 +397,7 @@ btnEstablecerBg.addEventListener("click",()=>{
 
 /*colores txt*/
 
-divInputColorTxt.addEventListener("change", (e) => {
-    cambiarColorTxtI();
-});
+divInputColorTxt.addEventListener("change", cambiarColorTxtI);
 
 redTxt.addEventListener("click", () => {
     cambiarColorTxt("red");
@@ -352,10 +433,7 @@ whiteTxt.addEventListener("click", () => {
 
 /*color bg*/
 
-
-// divInputColorBg.addEventListener("change", (e) => {
-//     cambiarBgTxtI();
-// });
+elegidoInputColorTxtBg.addEventListener("change", cambiarBgTxtI);
 
 redBg.addEventListener("click", () => {
     cambiarColorBgTxt("red");
@@ -388,6 +466,80 @@ blackBg.addEventListener("click", () => {
 whiteBg.addEventListener("click", () => {
     cambiarColorBgTxt("white");
 })
+
+// textos meme
+txtTopI.addEventListener("input", (e) =>{
+    
+    topT.innerText = e.target.value;
+});
+
+txtBottomI.addEventListener("input", (e) =>{
+    botT.innerText = e.target.value;
+    console.log(txtBottomI.value)
+});
+
+// textos ocultos 
+txtTopNone.addEventListener("click", ()=>{
+    contTxtTop.classList.toggle("close");
+})
+
+txtBottomNone.addEventListener("click", ()=>{
+    contTxtBottom.classList.toggle("close")
+ })
+
+//fondo transparente
+sinBg.addEventListener("click",transpFondo)
+
+//fuentes
+fuente.addEventListener("click",aplicarFuente)
+
+size.addEventListener("click",aplicarTamaño)
+
+//contorno fuentes
+btnNinguno.addEventListener("click", sinContorno)
+
+btnContClaro.addEventListener("click", contornoClaro);
+
+btnOscuro.addEventListener("click",contornoOscuro);
+
+//alinacion texto
+btnCentrado.addEventListener("click",()=>{
+    topT.style.textAlign="center";
+    botT.style.textAlign="center";
+});
+
+btnIzquierda.addEventListener("click",()=>{
+    topT.style.textAlign="left";
+    botT.style.textAlign="left";
+
+});
+
+btnDerecha.addEventListener("click",()=>{
+    topT.style.textAlign="right";
+    botT.style.textAlign="right";
+});
+
+//espaciado 
+espaciadoI.addEventListener("change", (e)=>{
+    let valorEspaciado = espaciadoI.value;
+    topT.style.letterSpacing = `${valorEspaciado}px`;
+    botT.style.letterSpacing = `${valorEspaciado}px`;
+});
+
+//interlineado
+const interl = (e) =>{
+
+    const valorInter = e.target.value;
+    console.log(valorInter)
+    txtTopI.style.lineHeight = valorInter;
+}
+
+interlineadoA.addEventListener("click",interl);
+interlineadoB.addEventListener("click",interl);
+interlineadoC.addEventListener("click",interl);
+interlineadoD.addEventListener("click",interl);
+interlineadoE.addEventListener("click",interl);
+interlineadoF.addEventListener("click",interl);
 
 // filtros
 brillo.addEventListener("change",()=>{
